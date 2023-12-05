fab17a9e4d3fc9a0419ccd63ce6011bcbe9372de
Rafhan Mazaya F <rafhan.faturahman@gmail.com>
2023-12-06 00:56:36 +0700

    Update Next Config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
-module.exports = {
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async headers() {
     return [
       {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET,DELETE,PATCH,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
@@ -21,3 +24,5 @@ module.exports = {
     ]
   },
 }
+
+module.exports = nextConfig
